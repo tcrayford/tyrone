25ad9004b83be0c4830996f5d9797b5330ed1a6e
Chris Lloyd <christopher.lloyd@gmail.com>
2009-07-31 09:10:22 +1000

    yo yo, here be some stylinz.

--- lib/tyrone.rb
+++ lib/tyrone.rb
@@ -24,14 +24,44 @@ __END__
 !!!
 %html
   %head
-    %title Mockups
+    %title mockups
     %style{:type => 'text/css', :media => 'screen'}
       :sass
+        
+        *
+          :margin 0
+          :padding 0
         body
-          :font-family Helvetica
+          :margin 40px
+          :font-family Helvetica, sans-serif
+        h1
+          :font-size 24px
+          :line-height 40px
+          :margin-bottom 20px
+          :text-transform lowercase
+        ul
+          :color #CCC
+          :font-size 12px
+          :line-height 20px
+          a
+            :text-decoration none
+            :font-size 14px
+            :color #333
+            :line-height 20px
+            :text-transform lowercase
+            &:hover
+              :border-bottom 1px solid #333
+        .footer
+          :font-size 11px
+          :line-height 20px
+          :margin-top 60px
+          :color #CCC
+          :display block
+          :text-decoration none
   %body
     %h1 Mockups
     %ul
       - @mockups.each do |mockup|
         %li
           %a{:href => mockup_path(mockup)}= mockup
+    %a.footer{:href => 'http://github.com/chrislloyd/tyrone'} tyrone luvs ya.
